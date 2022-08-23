07562e8658a8d68a0349856c62432b0438f3d189
sushipi <watachans1996@gmail.com>
2022-08-24 07:23:22 +0900

    AjaxAppの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
+  root to: 'posts#index' 
+  #get 'posts/new', to: 'posts#new' #新規投稿ページへの遷移は行わないため削除
   post 'posts', to: 'posts#create'
 end
